18b711a4843aecc824594dc4fc6a7a45d87961fa
anash6162-del <anash6162@gmail.com>
2025-11-17 12:39:34 +0545

    Add files via upload

---
+++ challenge1.py
@@ -0,0 +1,7 @@
+user_name=input('enter your full name:-' )
+step1=user_name.lower()
+step2=step1.strip()
+step3=step2.split()
+step4='_'.join(step3)
+print(step4)
+#variable_name.find('a') gives indexing!!
